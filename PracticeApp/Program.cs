d41f413b411482070fe797f0c2f572179baa35fd
Ziaul Islam <ziaul.islam@asos.com>
2018-08-04 14:32:39 +0100

    first commit simple write line

--- PracticeApp/Program.cs
+++ PracticeApp/Program.cs
@@ -6,7 +6,7 @@ namespace PracticeApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("test");
         }
     }
 }
