b0a2f38f029a6643175ffb04127cf1e709cf6ffb
Ziaul Islam <ziaul.islam@asos.com>
2018-08-04 15:24:21 +0100

    made a few arrays using strings, ints, objects and MultiDA's.
    looped through using a for loop. to extract the values within them.

--- PracticeApp/Program.cs
+++ PracticeApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using System.Globalization;
+using System.Linq;
 
 namespace PracticeApp
 {
@@ -6,7 +9,31 @@ namespace PracticeApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[3]; // explicitly defining my array length
+            arr[0] = 23;
+            string[] CustomerfirstName = {"Zee", "Adam", "Simon" };
+            var employees = new[] {"Drake", "Chris", "steve"};
+            object[] randomArr = {"Steve", 50, 1.234};
+
+            Console.WriteLine(" the value of index 0 is set to: {0}", arr[0]); // string interpiluiatation used to output the array index with the value.
+            Console.WriteLine(" the value of index 0 is set to: Name: {0} \n who is :{1} Years Old \n & has :{2} in his bank", randomArr); // string interpiluiatation used to output the array index with the value.
+
+            for (int i = 0; i < randomArr.Length; i++)
+            {
+                Console.WriteLine("array Index: {0}, Value: {1}", i, randomArr[i]); // looping through itterations of all values within the array and returning values 
+            }
+
+            string[,] customerNames = new string[2, 2] {{" Bob", "islam"} ,{"Zee", "Adam" }};
+            Console.WriteLine("Multi Dimention value:{0}",customerNames.GetValue(1,0));
+
+            for (int i = 0; i < customerNames.GetLength(0); i++)
+            {
+                for (int j = 0; j < customerNames.GetLength(1); j++)
+                {
+                    Console.Write("Multi Dimention value:{0}\n", customerNames.GetValue(i, j));
+                }
+            }
+            Console.ReadLine();
         }
-    }
+    } 
 }
